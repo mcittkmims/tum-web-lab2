c4fcfbfd1e254d66270a48be2fb788a156d815ed
mcittkmims <adrian.vremere.swe@gmail.com>
2026-03-17 17:03:06 +0200

    configure tailwind with custom theme

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://mcittkmims.github.io',
   base: '/tum-web-lab2',
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
